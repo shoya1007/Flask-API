02e3869e9db76b1d88206b67ac61c02b148d2b15
shoya1007 <shoya.fujisawa@rakus-partners.co.jp>
2021-06-09 09:50:15 +0900

    クエリパラメータでidを指定した場合の処理を追加

--- src/api.py
+++ src/api.py
@@ -9,14 +9,28 @@ def index():
     return jsonify({'test': 'Hello! This is City API'})
 
 
-@api.route('/city/', methods=['GET'])
+@api.route('/city', methods=['GET'])
 def fetch_cities():
-    cities = db.session.query(City).all()
-    if cities:
-        city_schema = CitySchema(many=True)
-        return jsonify(city_schema.dump(cities)), 200
+    # クエリパラメータを取得
+    req = request.args
+    # クエリパラメータがある場合
+    if req:
+        city_id = req.get('city_id')
+        city = db.session.query(City).filter(City.id == city_id).one()
+        if city:
+            city_schema = CitySchema()
+            return jsonify(city_schema.dump(city)), 200
+        else:
+            return jsonify({}), 404
+
+    # クエリパラメータがない場合
     else:
-        return jsonify({}), 404
+        cities = db.session.query(City).all()
+        if cities:
+            city_schema = CitySchema(many=True)
+            return jsonify(city_schema.dump(cities)), 200
+        else:
+            return jsonify({}), 404
 
 
 @api.route('/city/limit/', methods=['GET'])
